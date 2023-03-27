c00832018662ccd41f8dee5354069e675447178b
VINIA6 <viniciusdeassisazevedo@hotmail.com>
2023-03-27 16:54:10 -0300

    Feat: comunicação socket

---
+++ client/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import './App.css'
+import Join from './components/Join/Join'
+import Chat from './components/Chat/Chat'
+
+function App() {
+  const [chatVisibility, setChatVisibility ] = useState(false)
+
+  return (
+    <div className="App">
+      {
+        chatVisibility ? <Chat/> : <Join setChatVisibility={setChatVisibility}/>
+      }
+    </div>
+  )
+}
+
+export default App
